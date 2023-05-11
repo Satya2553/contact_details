68fd4c83abf42279dcffb4409fa21aaed4cdcee2
Satyanarayana <87627452+Satya2553@users.noreply.github.com>
2023-05-11 19:30:34 +0530

    Update settings.py

--- poc/poc/settings.py
+++ poc/poc/settings.py
@@ -139,5 +139,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'mareedu.satyanarayana.5475@gmail.com'
-EMAIL_HOST_PASSWORD = 'exwxkmfkittnmlcy'
+EMAIL_HOST_USER = '' //enter your email address
+EMAIL_HOST_PASSWORD = '' //enter your password
